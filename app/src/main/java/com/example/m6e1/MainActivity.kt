ac282549d324c2f8af2b967be550b33c9ef22ba6
Margaret <maenunezt@gmail.com>
2023-07-26 22:56:27 -0400

    Implementación de dependencias de navigation

--- app/src/main/java/com/example/m6e1/MainActivity.kt
+++ app/src/main/java/com/example/m6e1/MainActivity.kt
@@ -3,6 +3,20 @@ package com.example.m6e1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+[ ] Navigation
+[ ] Fragmento agregar
+[ ] Fragmento listado
+[ ] Crear interfaz
+[ ] Room
+    [ ] DAO
+    [ ] Database
+    [ ] Entity
+[ ] Fragmento listado
+[ ] Recycler View
+
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
