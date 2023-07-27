1c5929e6cba7432acf936c18bd89350372df6228
Margaret <maenunezt@gmail.com>
2023-07-27 10:52:51 -0400

    creación de TaskDataBase (Data Base de ROOM)

---
+++ app/src/main/java/com/example/m6e1/TaskDataBase.kt
@@ -0,0 +1,34 @@
+package com.example.m6e1
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database (entities = [Task:: class], version = 1)
+abstract class TaskDataBase : RoomDatabase(){
+
+    abstract fun getTaskDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TaskDataBase? = null
+
+        fun getDataBase(context: Context): TaskDataBase? {
+            val tempInstance = INSTANCE
+            if (tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                TaskDataBase:: class.java,
+                "task_database"
+                ).build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
